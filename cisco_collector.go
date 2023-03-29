ac6cbb43c7e7955223253871a931f1f8014cd7b3
Johann Wagner <johann.wagner@wobcom.de>
2023-03-29 19:07:54 +0200

    Added pool collector

--- cisco_collector.go
+++ cisco_collector.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gitlab.com/wobcom/cisco-exporter/pool"
 	"sync"
 	"time"
 
@@ -74,6 +75,7 @@ func newCiscoCollector(devices []*config.DeviceConfig, connectionManager *connec
 	pppoeCollector := pppoe.NewCollector()
 	mplsCollector := mpls.NewCollector()
 	natCollector := nat.NewCollector()
+	poolCollector := pool.NewCollector()
 
 	collectors[memoryCollector.Name()] = memoryCollector
 	collectors[cpuCollector.Name()] = cpuCollector
@@ -86,6 +88,7 @@ func newCiscoCollector(devices []*config.DeviceConfig, connectionManager *connec
 	collectors[pppoeCollector.Name()] = pppoeCollector
 	collectors[mplsCollector.Name()] = mplsCollector
 	collectors[natCollector.Name()] = natCollector
+	collectors[poolCollector.Name()] = poolCollector
 
 	for _, device := range devices {
 		for _, collectorName := range device.EnabledCollectors {
---
+++ pool/pool.go
@@ -0,0 +1,9 @@
+package pool
+
+type Pool struct {
+	Name string
+
+	AddressesTotal    float64
+	AddressesAvail    float64
+	AddressesAssigned float64
+}
