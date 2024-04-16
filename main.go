1ed1894bf6d00fb508cc86a81ea0cf15aa2b21dd
Julian Jacobi <julian.jacobi@wobcom.de>
2024-04-16 10:19:37 +0200

    Revert "Add profiling"
    This reverts commit 550391e63e724878d6f3184024a7c5b86d15b404.

--- main.go
+++ main.go
@@ -17,8 +17,6 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-
-    _ "net/http/pprof"
 )
 
 const version string = "1.4.0"
